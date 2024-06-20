0b892d6a51ae28bbd92176703870ed7ec95f0a91
Alex Chinedu <nwekealexchinedu@gmail.com>
2024-06-20 16:33:46 +0100

    create weather struct.

--- main.go
+++ main.go
@@ -6,8 +6,33 @@ import (
 	"net/http"
 )
 
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64   `json:"time_epoch"`
+				TempC     float64 `json:"temp_c"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
+
 func main() {
-	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=5edb5652cbae4f958cc115437242006&q=Port-harcourt&aqi=no&days=1&alerts=no")
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=5edb5652cbae4f958cc115437242006&q=Port-harcourt&aqi=no&days=1&alerts=no")
 	if err != nil {
 		panic(err)
 	}
