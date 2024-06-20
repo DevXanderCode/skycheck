f894bc584d3e8a8394a968a4465e0567efa7c8d0
Alex Chinedu <nwekealexchinedu@gmail.com>
2024-06-20 13:02:57 +0100

    feat: add weather api.

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("ready to go!!!")
+	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=5edb5652cbae4f958cc115437242006&q=Port harcourt&aqi=no&days=1&alerts=no")
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather Api not available")
+	}
 }
